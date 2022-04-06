f7e60936e774f24c65194ffec9660a66e6a1ecc8
Andrew Mushel <andrew.mushel@unstoppabledomains.com>
2022-04-06 11:57:09 -0500

    JS script for opening external links in new tabs

---
+++ static/scripts/etlb.js
@@ -0,0 +1,17 @@
+function eltb_click_callback(event) {
+    let link = null;
+    // Handle click event targets that are children of the <a> tag we want to check
+    for (let path_element of event.path) {
+        if (path_element.tagName == "A") {
+            link = path_element;
+            break;
+        } 
+        else if (path_element.tagName == "ARTICLE") break;
+    }
+
+    if ((link != null) && /^(http.?):\/\//.test(link.href)) { // isn't a relative or absolute link
+        if(window.location.origin != link.origin) link.target = "_blank"; // Link doesn't have same origin as current page
+    }
+}
+
+window.addEventListener("click", eltb_click_callback);
