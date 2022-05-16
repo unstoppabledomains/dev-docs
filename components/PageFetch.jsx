bec70ed2e6fe5feaed2992039a68bd622bcdf6a4
Andrew Mushel <andrew.mushel@unstoppabledomains.com>
2022-05-16 09:43:03 -0500

    Questionable hack to generate redocly TOC from fetched React-Markdown

--- components/PageFetch.jsx
+++ components/PageFetch.jsx
@@ -1,4 +1,5 @@
-import React, {useEffect, useState} from 'react';
+import React, {createElement, useEffect, useState} from 'react';
+import {flushSync} from 'react-dom';
 import ReactMarkdown from 'react-markdown';
 import rehypeRaw from 'rehype-raw';
 import rehypeSanitize from 'rehype-sanitize';
@@ -30,12 +31,18 @@ export function FetchMD(props) {
 
     const fetchContent = async (resource) => {
         let text = await fetchFileText(resource);
+        //document.querySelector('#gatsby-focus-wrapper > div > div > div > div > section > article'); // Page content
         setContent(text);
     }
 
-    return (
-        <ReactMarkdown rehypePlugins={plugins}>{content}</ReactMarkdown>
-    )
+    let rmd = ReactMarkdown({
+            components: {h1: 'h2', h2: HeadingRenderer, h3: HeadingRenderer, h4: HeadingRenderer, h5: HeadingRenderer, h6: HeadingRenderer},
+            rehypePlugins: {plugins}, 
+            children: content });
+
+    if (props.toc === 'true') dynamicRenderTOC(rmd);
+    
+    return rmd;
 }
 
 async function fetchFileText(resource) {
@@ -46,4 +53,37 @@ async function fetchFileText(resource) {
     let text = await blob.text();
 
     return text;
+}
+
+function dynamicRenderTOC(rmd) {
+    const toc = document.querySelector('#gatsby-focus-wrapper > div > div > div > div > aside > div');
+    if (toc) {
+        let tocTitle = toc.querySelector('div');
+        let titleClass = tocTitle ? tocTitle.className : '';
+
+        let hElements = toc.querySelectorAll('a');
+        let hClassNames = [];
+        for (let heading of hElements) {
+            hClassNames.push(heading.className);
+        }
+
+        toc.innerHTML = '<div class =\"' + titleClass + '\" >On this page<div>\n';
+
+        let headings = '';
+        for (let element of rmd.props.children) {
+            if (element.type === HeadingRenderer) {
+                headings += '<a ' +
+                            'href=\"#' + element.props.children[0] + '\" ' +
+                            'data-cy=\"' + 'toc-' + element.props.children[0] + '\"' +
+                            'class =\"' + hClassNames[element.props.level - 1] + '\" >' + 
+                            element.props.children[0] + 
+                            '</a>\n';
+            }
+        }
+        toc.innerHTML += headings;
+    }
+}
+
+function HeadingRenderer({node, ...props}) {
+    return React.createElement('h'+props.level, {id: props.children[0]}, props.children);
 }
