a1074d852d1d129bad2563e66bf865e5bb14e7e2
Andrew Mushel <andrew.mushel@unstoppabledomains.com>
2022-10-28 16:29:50 -0500

    Add Freshdesk feedback widget

---
+++ static/scripts/fd-widget.js
@@ -0,0 +1,5 @@
+window.fwSettings = {
+    'widget_id':48000002534
+};
+
+!function(){if("function"!=typeof window.FreshworksWidget){var n=function(){n.q.push(arguments)};n.q=[],window.FreshworksWidget=n}}()
