ee8b9e302b50ba65e417772f36aa6596f50a383c
Andrew Mushel <andrew.mushel@unstoppabledomains.com>
2023-01-04 12:53:54 -0600

    Optional figcaption support for Video component

--- components/Video.tsx
+++ components/Video.tsx
@@ -5,27 +5,28 @@ export default function Video(props) {
     let parsed_width = parse_dimension_value(props?.width);
     let parsed_height = parse_dimension_value(props?.height);
 
-    // Set video-container styles
+    // Retrieve and/or calculate video-container styles
     let container_styles : React.CSSProperties = {margin: props.centered ? "auto" : ""};
 
     if (parsed_width) container_styles["--video-max-width"] = parsed_width;
     if (props?.type === "iframe") {
+        let parsed_ratio = parseFloat(props?.ratio);
+        if (!isNaN(parsed_ratio)) 
+            container_styles["--video-aspect-ratio"] = parsed_ratio;
         // Only calculate custom aspect ratio from pixel-based height and width
-        if (parsed_width.indexOf("px") > 0 && parsed_height.indexOf("px") > 0) {
+        else if (parsed_width.indexOf("px") > 0 && parsed_height.indexOf("px") > 0) {
             container_styles["--video-aspect-ratio"] = `calc(${parseFloat(parsed_height)} / ${parseFloat(parsed_width)})`;
-        }
-    
-        // Override calculation if specific ratio is specified
-        let parsed_ratio = parseFloat(props.ratio);
-        if (!isNaN(parsed_ratio)) container_styles["--video-aspect-ratio"] = parsed_ratio;
-        
+        }        
     } else if (props?.type === "video") {
         container_styles.height = "auto";
         container_styles.paddingBottom = "0";
     }
 
-    let embed = 
-        props.type === "iframe" ?
+    // Create video container and select embed type
+    let video_container = (
+        <div className={props.type ? "video-container" : ""} style={container_styles}>
+
+        { props.type === "iframe" ?
             <iframe
                 src = {props.src}
                 title = {props?.title} 
@@ -42,15 +43,32 @@ export default function Video(props) {
             />
         :
             <div style={{color: "red", textAlign: "center"}}>Invalid type property. Specify either "iframe" or "video."</div>
-    ;
-
-    return (
-        <div>
-            <div className={props.type ? "video-container" : ""} style={container_styles}>
-                {embed}
-            </div>
+        }
         </div>
-    )
+    );
+
+    // Retrieve the figure caption
+    let caption = props?.title;
+    if (!caption && props?.children) {
+        caption = "";
+        for (let child of props?.children) {
+            if (typeof child === "string" || child instanceof String) {
+                caption += child;
+            }
+        }
+    }
+    
+    return (
+        caption ?
+            <figure>
+                {video_container}
+                <figcaption>{caption}</figcaption>
+            </figure>   
+        :
+            <div>
+                {video_container}
+            </div>
+    );
 }
 
 function parse_dimension_value(dim : string) : string {
