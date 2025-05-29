65a737b6d9f1a9634c40569240562e41d311b8c7
Vincent Shadbolt <vincent@unstoppabledomains.com>
2025-05-29 09:41:03 -0700

    chore: Landing Page Redesign
    * Redesigned docs landing page
    - fixed console errors from developer survey
    - added rotating quotes
    
    * Added some important tags to css and fixed quote sizing
    
    * Updated domain count to generic millions

--- components/DeveloperSurvey.tsx
+++ components/DeveloperSurvey.tsx
@@ -3,15 +3,95 @@ import * as React from 'react';
 export function DeveloperSurvey() {
   return (
     <div className="margin-top-bottom-30">
-      <div style={{ backgroundColor: '#0D67FE', color: '#FFFCF0', padding: '4rem', borderRadius: '1.5rem', textAlign: 'center' }}>
-        <h2 style={{ fontWeight: 600, fontSize: 31.5, marginTop: '0px' }}>
+      <div style={{ 
+        backgroundColor: '#0D67FE', 
+        color: '#FFFCF0', 
+        padding: '2rem',
+        borderRadius: '1.5rem', 
+        textAlign: 'center',
+        maxWidth: '100%',
+        overflow: 'hidden'
+      }}>
+        <h2 style={{ 
+          fontWeight: 600, 
+          fontSize: 'clamp(24px, 5vw, 31.5px)', 
+          marginTop: '0px',
+          marginBottom: '1rem'
+        }}>
           Help us improve our products!
         </h2>
-        <p style={{ lineHeight: '25px' }}>
+        <p style={{ 
+          lineHeight: '25px',
+          maxWidth: '800px',
+          margin: '0 auto',
+          padding: '0 1rem'
+        }}>
           We're always looking for ways to improve how developers use and integrate our products into their applications. We'd love to hear about your experience to help us get better.
         </p>
-        <button data-Tf-Slider={ "uHPQyHO6" } data-Tf-Width={ 550 } data-Tf-Iframe-Props={ "title=Developer Research Survey" } data-Tf-Medium={ "snippet" } style={{ all: 'unset', display: 'inline-block', maxWidth: '100%', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', fontSize: '16px', borderRadius: '4px', padding: '10.5px 44px', fontWeight: '600', height: '35px', cursor: 'pointer', lineHeight: '35px', textDecoration: 'none', border: '2px solid #FFFCF0', margin: '0.5em 1em 0.5em 1em' }}>Take Our Developer Survey</button>
-        <button data-Tf-Slider={ "OC87toiF" } data-Tf-Width={ 550 } data-Tf-Iframe-Props={ "title=Developer Research Survey" } data-Tf-Medium={ "snippet" } style={{ all: 'unset', display: 'inline-block', maxWidth: '100%', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', fontSize: '16px', borderRadius: '4px', padding: '10.5px 44px', fontWeight: '600', height: '35px', cursor: 'pointer', lineHeight: '35px', textDecoration: 'none', border: '2px solid #FFFCF0', margin: '0.5em 1em 0.5em 1em' }}>Take Our Partner API Survey</button>
+        <div style={{
+          display: 'flex',
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+          gap: '1rem',
+          justifyContent: 'center',
+          marginTop: '2rem',
+          width: '100%'
+        }}>
+          <button 
+            data-tf-slider="uHPQyHO6" 
+            data-tf-width="550" 
+            data-tf-iframe-props="title=Developer Research Survey" 
+            data-tf-medium="snippet" 
+            style={{ 
+              all: 'unset', 
+              display: 'inline-block', 
+              flex: '1 1 250px',
+              minWidth: '250px',
+              maxWidth: '300px',
+              whiteSpace: 'nowrap', 
+              overflow: 'hidden', 
+              textOverflow: 'ellipsis', 
+              fontSize: '16px', 
+              borderRadius: '4px', 
+              padding: '10.5px 20px', 
+              fontWeight: '600', 
+              height: '35px', 
+              cursor: 'pointer', 
+              lineHeight: '35px', 
+              textDecoration: 'none', 
+              border: '2px solid #FFFCF0'
+            }}
+          >
+            Take Our Developer Survey
+          </button>
+          <button 
+            data-tf-slider="OC87toiF" 
+            data-tf-width="550" 
+            data-tf-iframe-props="title=Developer Research Survey" 
+            data-tf-medium="snippet" 
+            style={{ 
+              all: 'unset', 
+              display: 'inline-block', 
+              flex: '1 1 250px',
+              minWidth: '250px',
+              maxWidth: '300px',
+              whiteSpace: 'nowrap', 
+              overflow: 'hidden', 
+              textOverflow: 'ellipsis', 
+              fontSize: '16px', 
+              borderRadius: '4px', 
+              padding: '10.5px 20px', 
+              fontWeight: '600', 
+              height: '35px', 
+              cursor: 'pointer', 
+              lineHeight: '35px', 
+              textDecoration: 'none', 
+              border: '2px solid #FFFCF0'
+            }}
+          >
+            Take Our Partner API Survey
+          </button>
+        </div>
       </div>
     </div>
   );
